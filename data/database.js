3c638e98086cac186baaeca99970be8d5d040df9
techarm <techarmdev@gmail.com>
2023-01-11 22:34:40 +0900

    Update env variable name

--- data/database.js
+++ data/database.js
@@ -1,11 +1,11 @@
 import { MongoClient } from 'mongodb';
 
-const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
+const dbHost = process.env.MONGODB_HOST;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
