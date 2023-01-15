17f4b00474b1160d776eaa1228552ad5e5148573
techarm <techarmdev@gmail.com>
2023-01-15 15:02:54 +0900

    Update python script

--- .github/actions/web-grep-docker/command.py
+++ .github/actions/web-grep-docker/command.py
@@ -6,14 +6,15 @@ def run():
     url = os.environ["INPUT_URL"]
     match_string = os.environ["INPUT_MATCH-STRING"]
 
-    try:
-        res = requests.get(url)
-        match_list = re.findall(match_string, res.text)
-        print(f'is-match={len(match_list) > 0}', file=gh_output)
-        print(f'result={match_list}', file=gh_output)
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as gh_output:
+        try:
+            res = requests.get(url)
+            match_list = re.findall(match_string, res.text)
+            print(f'is-match={len(match_list) > 0}', file=gh_output)
+            print(f'result={match_list}', file=gh_output)
 
-    except Exception as e:
-        print(f'error={e}', file=gh_output)
+        except Exception as e:
+            print(f'error={e}', file=gh_output)
 
 if __name__ == '__main__':
     run()
